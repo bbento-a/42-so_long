87fb0f93e451699294ff67843b21f602bdf26ede
bbento-a <bbento-a@student.42lisboa.com>
2024-06-24 12:46:57 +0100

    Working on Makefile

--- Makefile
+++ Makefile
@@ -6,7 +6,43 @@
 #    By: bbento-a <bbento-a@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/21 15:37:59 by bbento-a          #+#    #+#              #
-#    Updated: 2024/06/21 15:38:00 by bbento-a         ###   ########.fr        #
+#    Updated: 2024/06/24 12:42:33 by bbento-a         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+# VARIABLES
+
+# Name of the executable(s)
+
+NAME		= so_long
+
+# Directories
+
+LIBFT		= ./libft/libft.a
+INC			= inc/
+SRC_DIR		= src/
+OBJ_DIR		= obj/
+
+# Compiler and Flags
+CC			= cc
+CFLAGS		= -Wall -Wextra -Werror -g
+RM			= rm -f
+
+# Source Files
+
+SOLONG_SRCS = $(SRC_DIR)
+
+# Conversion of files (Linking and applying pattern substituition to all source files)
+
+SRC			= $(SOLONG_SRCS)
+OBJ			= $(patsubst $(SRC_DIR).c,$(OBJ_DIR).o, $(SRCS))
+
+# RULES
+
+$(LIBFT)
+			make -C ./libft
+
+all: $(NAME)
+
+$(NAME)		$(OBJ) $(LIBFT)
+			$(CC) $(CFLAGS) $()
---
+++ inc/so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbento-a <bbento-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 12:12:09 by bbento-a          #+#    #+#             */
+/*   Updated: 2024/06/24 12:19:04 by bbento-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <limits.h>
+# include "../libft/libft.h"
+# include "../libft/ft_printf.h"
+# include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx_int.h"
+
+
+
+
+#endif
